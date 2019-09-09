0c255693a60bf64d1ab8f7899dc3b85576ba86b6
sa-js <philipthevictor@gmail.com>
2019-09-09 20:13:12 +0500

    done 1.9: unicafe step4

--- part1/a_more_complex_state_debugging_react_apps/part1-app/src/index.js
+++ part1/a_more_complex_state_debugging_react_apps/part1-app/src/index.js
@@ -2,64 +2,66 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Statistics = ({good, neutral, bad, value}) => {
+const Statistics = ({ good, neutral, bad, value }) => {
 
     const checkNaN = (value) => {
         return Number.isNaN(value) ? 0 : value;
     }
 
     return (
-        <>
-          <h3>Statistics</h3>
-            <p>good : {good}</p>
-            <p>neutral : {neutral}</p>
-            <p>bad : {bad}</p>
-            <p>all : {good+bad+neutral}</p>
-            <p>average : {checkNaN(value/(good+bad+neutral))}</p>
-            <p>positive : {checkNaN(good/(good+neutral+bad))*100}%</p>
+        <> {good + bad + neutral > 0 ?
+            (<div><h3>Statistics</h3>
+                <p>good : {good}</p>
+                <p>neutral : {neutral}</p>
+                <p>bad : {bad}</p>
+                <p>all : {good + bad + neutral}</p>
+                <p>average : {checkNaN(value / (good + bad + neutral))}</p>
+                <p>positive : {checkNaN(good / (good + neutral + bad)) * 100}%</p></div>)
+            : <div><br />No feedback given</div>
+        }
         </>
     )
 }
 
-const Button = ({ onClick,text }) => (
+const Button = ({ onClick, text }) => (
     <>
         <button onClick={onClick} >{text}</button>
     </>
 )
 
 const App = (props) => {
-    const [ good, setGood ] = useState(0);
-    const [ neutral, setNeutral ] = useState(0);
-    const [ bad, setBad ] = useState(0);
-    const [ value, setValue ] = useState(0);
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [value, setValue] = useState(0);
 
 
     const setGoodFunction = () => {
-        setGood(good+1);
-        setValue(value+1);
+        setGood(good + 1);
+        setValue(value + 1);
     }
 
     const setNeutralFunction = () => {
-        setNeutral(neutral+1);
-        setValue(value+0);
+        setNeutral(neutral + 1);
+        setValue(value + 0);
 
     }
 
     const setBadFunction = () => {
-        setBad(bad+1);
-        setValue(value-1);
+        setBad(bad + 1);
+        setValue(value - 1);
     }
 
     return (
         <div>
             <h1>give feedback</h1>
-        <div>
-            <Button onClick={setGoodFunction} text="good"/>
-            <Button onClick={setNeutralFunction} text="neutral"/>
-            <Button onClick={setBadFunction} text="bad"/>
-            <Statistics good={good} neutral={neutral} bad={bad} value={value} />
+            <div>
+                <Button onClick={setGoodFunction} text="good" />
+                <Button onClick={setNeutralFunction} text="neutral" />
+                <Button onClick={setBadFunction} text="bad" />
+                <Statistics good={good} neutral={neutral} bad={bad} value={value} />
+            </div>
         </div>
-      </div>
     )
 }
 
