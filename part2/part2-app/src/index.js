2b4b476546b736abf021d1e5dace557060e0f4b4
sa-js <philipthevictor@gmail.com>
2019-09-19 09:17:15 +0500

    2.1: course contents step6

---
+++ part2/part2-app/src/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header name={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      {props.parts.map((v, i) => {
+        return <Part key={v.id} v={v} />
+      })
+      }
+
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.v.name} : {props.v.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
